7d21097abec76f7a23b00178d68e71894511ee3a
Tasso Evangelista <tasso.evangelista@rocket.chat>
2019-12-23 21:45:56 -0300

    Update QA parameters

--- client/components/setupWizard/steps/RegisterServerStep.js
+++ client/components/setupWizard/steps/RegisterServerStep.js
@@ -176,7 +176,7 @@ export function RegisterServerStep({ step, title, active }) {
 					<Label text={<>{t('Register_Server_Registered_I_Agree')} <a href='https://rocket.chat/terms'>{t('Terms')}</a> & <a href='https://rocket.chat/privacy'>{t('Privacy_Policy')}</a></>} position='end' className='SetupWizard__RegisterServerStep__PrivacyTerms'>
 						<CheckBox
 							name='agreeTermsAndPrivacy'
-							data-qa-agree-terms
+							data-qa='agree-terms-and-privacy'
 							disabled={!registerServer}
 							checked={agreeTermsAndPrivacy}
 							onChange={({ currentTarget: { checked } }) => {
